c772235e3edacb0233b0f6ba2c3c68a982b1b526
Mohamed Elerian <103853528+Elerian07@users.noreply.github.com>
2023-01-08 02:25:18 +0200

    wish

--- src/modules/wishList/wishList.router.js
+++ src/modules/wishList/wishList.router.js
@@ -5,7 +5,9 @@ import { endpoints } from './wishList.endPoints.js';
 import * as wishListController from './controller/wishList.controller.js';
 const router = Router({ mergeParams: true })
 
-
+app.get("/", (req, res) => {
+    res.send("<h1>Wishlist Page</h1 > <br> <h5>By Mohamed Elerian</h5>")
+})
 
 router.put("/", auth(endpoints.create), wishListController.addWishList)
 router.put("/remove", auth(endpoints.remove), wishListController.removeWishList)
